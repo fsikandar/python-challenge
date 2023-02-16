7a95dc20447f1e5233995d5169183da1f60c5886
Feeza Sikandar <fhsikandar@gmail.com>
2023-02-16 21:54:46 +0000

    new

---
+++ PyBank/RESOURCES1/main_pybank.py
@@ -0,0 +1,59 @@
+# pybank analysis
+import os
+import csv
+import pandas as pd
+import numpy as np
+
+pd.set_option('display.max_rows', 500)
+# reading csvfile using pandas
+budget_file = 'C:/Users/sikan/ub/homework/02_VBA/challenge_3/Instructions/PyBank/Resources/budget_data.csv'
+df = pd.read_csv(budget_file) 
+
+# printing the header
+header = df.columns
+print(f'HEADER: {header}')
+
+# counting total months
+
+total = 0
+
+# Count values in the 'Date' column 
+total += df['Date'].count() 
+
+# Print total 
+total_months = total
+print(f'TOTAL_months: {total_months}')
+# net total amount of "Profit/Losses" over the entire period
+
+net_total = 0
+net = df['Profit/Losses'].sum() 
+print(f'Total: ${net}')
+#Calculate the average change in revenue between months over the entire period
+
+df['change'] = df['Profit/Losses'].diff()
+df['change'].mean()
+
+# avg_change only upto 2 decimals
+avg_change = '%.2f'% df['change'].mean()
+print(f'Average change: $ {avg_change}')
+# printing dataframe for visualisation
+#print(df)
+# finding min and max change
+max_change = df['change'].max()
+min_change = df['change'].min()
+print(f'max_change: ${max_change}')
+print(f'min_change: ${min_change}')
+
+#finding month associated with min and max
+print(df.loc[df['change']==1862002.0])
+print('Greatest increase in profits: Aug-16 ($1862002.0)')
+
+print(df.loc[df['change']==-1825558.0])
+print('Greatest decrease in profits: Feb-14 (-$1825558.0)')
+
+
+
+
+
+
+
--- PyPoll/main_pypoll.py
+++ PyPoll/main_pypoll.py
@@ -1,1 +1,52 @@
 # pypoll analysis
+import os
+import csv
+
+import pandas as pd
+pd.set_option('display.max_rows', 500)
+
+election_file ='C:/Users/sikan/ub/homework/02_VBA/challenge_3/Instructions/PyPoll/Resources/election_data.csv'
+# read csvfile
+df = pd.read_csv(election_file )
+    
+# print header
+header = df.columns
+print(f'HEADER: {header}')
+
+# no. of voters
+voters_count = df['Ballot ID'].count()
+
+print(f'VOTERS:{voters_count}')
+
+#print(df)
+#finding candidate names
+candidates = df['Candidate'].unique()
+print(candidates)
+
+#counting number of votes for each candidate
+
+candidate_1 = df['Candidate'].value_counts()['Charles Casper Stockham']
+candidate_2 = df['Candidate'].value_counts()['Diana DeGette']
+candidate_3 = df['Candidate'].value_counts()['Raymon Anthony Doane']
+
+# finding percentage for each candidate 
+
+p_1 = '%.3f'% ((candidate_1/ voters_count)*100)
+p_2 = '%.3f'% ((candidate_2/ voters_count)*100)
+p_3 = '%.3f'% ((candidate_3/ voters_count)*100)
+
+# printing the results
+
+print(p_1)
+print(p_2)
+print(p_3)
+print(f'Charles Casper Stockham : votes ({candidate_1})   {p_1}% ')
+print(f'Diana DeGette :votes ({candidate_2})  {p_2}%')
+print(f'Raymon Anthony Doane :votes ({candidate_3})  {p_3}% ')
+
+#finding winner
+list1 = [23.049, 73.812, 3.139]
+print(max(list1))
+
+print(f'winner: Diana DeGette ')
+print(max(list1))
